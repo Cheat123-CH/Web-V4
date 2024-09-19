c99e0114cbfe4f22ff3e169caa06dcdbc5d3f59c
CHAN Suvannet <chansuvannet999@gmail.com>
2024-09-19 12:12:32 +0700

    change login form

--- src/app/resources/account/auth/auth.component.ts
+++ src/app/resources/account/auth/auth.component.ts
@@ -15,6 +15,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from 'app/core/auth/auth.service';
+import { LanguagesComponent } from 'app/layout/common/languages/languages.component';
 import { helperAnimations } from 'helper/animations';
 import { HelperAlertComponent, HelperAlertType } from 'helper/components/alert';
 
@@ -35,6 +36,7 @@ import { HelperAlertComponent, HelperAlertType } from 'helper/components/alert';
         MatIconModule,
         MatCheckboxModule,
         MatProgressSpinnerModule,
+        LanguagesComponent,
     ],
 })
 export class AuthSignInComponent implements OnInit {
@@ -44,6 +46,14 @@ export class AuthSignInComponent implements OnInit {
         type: 'success',
         message: '',
     };
+    images: string[] = [
+        'images/apps/pos1.png',
+        'images/apps/pos1.png',
+        'images/apps/pos1.png',
+    ];
+    currentImage: string = this.images[0];
+    imageIndex: number = 0;
+    interval: any;
     signInForm: UntypedFormGroup;
     showAlert: boolean = false;
 
@@ -69,6 +79,7 @@ export class AuthSignInComponent implements OnInit {
             username: ['0889566929', [Validators.required, Validators.pattern('^0[0-9]{8,9}$')]],
             password: ['123456', Validators.required]
         });
+        this.startImageSlider();
     }
 
     // -----------------------------------------------------------------------------------------------------
@@ -113,5 +124,19 @@ export class AuthSignInComponent implements OnInit {
                 this.showAlert = true;
             }
         });
+    }
+
+    startImageSlider() {
+        this.interval = setInterval(() => {
+            this.imageIndex = (this.imageIndex + 1) % this.images.length;
+            this.currentImage = this.images[this.imageIndex];
+        }, 100000); // 100000 milliseconds = 100 seconds
+    }
+
+    ngOnDestroy() {
+        // Clear the interval when the component is destroyed to prevent memory leaks
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
     }
 }
