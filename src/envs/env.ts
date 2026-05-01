2abf1c08a0ba4b03483fd12be681620be369bb1d
Cheat123-CH <sokhcheatmet2024@gmail.com>
2026-05-01 12:08:46 +0700

    fix web

--- src/envs/env.ts
+++ src/envs/env.ts
@@ -3,5 +3,5 @@ export const env = {
     production: true,
     API_BASE_URL: process.env.API_BASE_URL,
     FILE_BASE_URL: process.env.FILE_BASE_URL,
-    WEB_BASE_URL: 'https://file-v4-api.uat.camcyber.com/'
+    WEB_BASE_URL: 'https://web-v4.onrender.com'
 };
