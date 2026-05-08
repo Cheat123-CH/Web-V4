8896bae1cec9035f93c491938be1ba72f5b5b977
Cheat123-CH <sokhcheatmet2024@gmail.com>
2026-05-08 16:24:55 +0700

    fix: set production API URLs

--- src/envs/env.ts
+++ src/envs/env.ts
@@ -1,7 +1,7 @@
 export const env = {
 
     production: true,
-    API_BASE_URL: 'https://api-v4-ortr.onrender.com/api',
+    API_BASE_URL: 'https://api-v4-ortr.onrender.com',
     FILE_BASE_URL: 'https://file-v3-r1b9.onrender.com/',
     WEB_BASE_URL: 'https://file-v4-api.uat.camcyber.com/',
     SOCKET_URL: 'https://api-v4-ortr.onrender.com/'
